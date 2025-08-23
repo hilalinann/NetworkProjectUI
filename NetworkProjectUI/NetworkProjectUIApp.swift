62d39b0dc056e7a7c2a48e47abc7f92afa178375
Hilal İnan <hilal.inan@arabam.com>
2025-08-23 16:27:15 +0300

    Initial Commit

---
+++ NetworkProjectUI/NetworkProjectUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkProjectUIApp.swift
+//  NetworkProjectUI
+//
+//  Created by Hilal İnan on 23.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct NetworkProjectUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
