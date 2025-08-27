f579265ebdb9abf4b1c1e378fb70bb0dbe12418f
Hilal İnan <hilal.inan@arabam.com>
2025-08-27 15:05:35 +0300

    client update

--- NetworkProjectUI/Network/Base/Client.swift
+++ NetworkProjectUI/Network/Base/Client.swift
@@ -18,42 +18,63 @@ public final class Client: ClientProtocol {
     
     // MARK: - Fetch
     public func fetch<T: Decodable>(request: Request) async -> Result<T, NetworkError> {
-        do {
-            return try await fetch(request)
-        } catch let error as NetworkError {
-            //TODO: Alert presenting
-            return .failure(error)
-        } catch {
-            //TODO: Alert presenting
-            return .failure(NetworkError(type: .unknown, message: "Unknown Error"))
-        }
-    }
-    
-    private func fetch<T: Decodable>(_ request: Request) async throws -> Result<T, NetworkError> {
         
         guard let urlRequest = request.buildURLRequest() else {
-            return .failure(NetworkError(type: .notFound, message: "Invalid URL")) //Geçersiz URL
+            return .failure(NetworkError(type: .notFound, message: "Invalid URL"))
         }
-        
         do {
             let (data, response) = try await session.data(for: urlRequest)
-            
             guard let urlResponse = response as? HTTPURLResponse else {
-                return .failure(NetworkError(type: .badRequest, message: "Bad Request")) //istemci HTTP yanıtı alamadı.
+                return .failure(NetworkError(type: .badRequest, message: "Bad Request"))
             }
-            
             guard (200...299).contains(urlResponse.statusCode) else {
-                return .failure(NetworkError(type: .noResponse, message: "No Response")) //http isteği yapıldı ama başarılı yanıt yok
+                return .failure(NetworkError(type: .noResponse, message: "No Response"))
             }
-            
-            let decodedResponse = try JSONDecoder().decode(T.self, from: data)
-            return .success(decodedResponse)
+            let decoded = try JSONDecoder().decode(T.self, from: data)
+            return .success(decoded)
         } catch let error as DecodingError {
             return .failure(NetworkError.handleDecodingError(error: error))
         } catch {
             return .failure(NetworkError(type: .unknown, message: "Unknown Error"))
         }
     }
+//    public func fetch<T: Decodable>(request: Request) async -> Result<T, NetworkError> {
+//        do {
+//            return try await fetch(request)
+//        } catch let error as NetworkError {
+//            //TODO: Alert presenting
+//            return .failure(error)
+//        } catch {
+//            //TODO: Alert presenting
+//            return .failure(NetworkError(type: .unknown, message: "Unknown Error"))
+//        }
+//    }
+    
+//    private func fetch<T: Decodable>(_ request: Request) async throws -> Result<T, NetworkError> {
+//        
+//        guard let urlRequest = request.buildURLRequest() else {
+//            return .failure(NetworkError(type: .notFound, message: "Invalid URL")) //Geçersiz URL
+//        }
+//        
+//        do {
+//            let (data, response) = try await session.data(for: urlRequest)
+//            
+//            guard let urlResponse = response as? HTTPURLResponse else {
+//                return .failure(NetworkError(type: .badRequest, message: "Bad Request")) //istemci HTTP yanıtı alamadı.
+//            }
+//            
+//            guard (200...299).contains(urlResponse.statusCode) else {
+//                return .failure(NetworkError(type: .noResponse, message: "No Response")) //http isteği yapıldı ama başarılı yanıt yok
+//            }
+//            
+//            let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+//            return .success(decodedResponse)
+//        } catch let error as DecodingError {
+//            return .failure(NetworkError.handleDecodingError(error: error))
+//        } catch {
+//            return .failure(NetworkError(type: .unknown, message: "Unknown Error"))
+//        }
+//    }
 }
 
 public protocol ClientProtocol: Sendable {
